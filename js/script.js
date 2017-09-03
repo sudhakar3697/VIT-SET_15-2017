9d5250cd4c4dacf2a7c332b6b5ae847be1dbc19f
Vikram Adithya Ganesh <callvikram22@gmail.com>
2017-09-03 16:16:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
         $('.slider').slider();
-		$(".button-collapse").sideNav();
+		$('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'right',
+      closeOnClick: true,
+      draggable: true,
+    }
+  );
+
 		$('.collapsible').collapsible();
 		 $('.modal').modal();	
 		  $('.materialboxed').materialbox();
@@ -14,7 +21,7 @@ $(document).ready(function(){
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
         document.getElementById("contactUs").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
     }
     function showCallForPapers(){ 
         document.getElementById("home").style.display="none";
@@ -24,7 +31,7 @@ $(document).ready(function(){
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
         document.getElementById("contactUs").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
     }
     function showPaperSubmission(){
         document.getElementById("home").style.display="none";
@@ -34,7 +41,7 @@ $(document).ready(function(){
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
         document.getElementById("contactUs").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
     }
     function showRegistration(){
          
@@ -45,7 +52,7 @@ $(document).ready(function(){
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
         document.getElementById("contactUs").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
     }
     function showCommittee(){  
         document.getElementById("home").style.display="none";
@@ -55,7 +62,7 @@ $(document).ready(function(){
         document.getElementById("committee").style.display="block";
         document.getElementById("accommodation").style.display="none";
         document.getElementById("contactUs").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
     }
     function showAccommodation(){
         document.getElementById("home").style.display="none";
@@ -64,7 +71,7 @@ $(document).ready(function(){
         document.getElementById("reg").style.display="none";
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="block";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
         document.getElementById("contactUs").style.display="none";
     }
     function showcontactUs(){
@@ -74,16 +81,16 @@ $(document).ready(function(){
         document.getElementById("reg").style.display="none";
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
-		document.getElementById("brochure").style.display="none";
+	document.getElementById("brochure").style.display="none";
         document.getElementById("contactUs").style.display="block";
     }
 	function showbro(){
-		document.getElementById("home").style.display="none";
+	document.getElementById("home").style.display="none";
         document.getElementById("callForPapers").style.display="none";
         document.getElementById("paperSubmission").style.display="none";
         document.getElementById("reg").style.display="none";
         document.getElementById("committee").style.display="none";
         document.getElementById("accommodation").style.display="none";
-		document.getElementById("brochure").style.display="block";
+	document.getElementById("brochure").style.display="block";
         document.getElementById("contactUs").style.display="none";
 	}	
